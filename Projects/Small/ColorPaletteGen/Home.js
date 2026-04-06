61a90b9fed0d138623ddd4c114636c6979671f8e
m4shaikh <moin.shaikh.3656@gmail.com>
2026-04-07 02:19:49 +0530

    New Small Project Initiated

---
+++ Projects/Small/ColorPaletteGen/Home.js
@@ -0,0 +1,14 @@
+let introCard = document.getElementById('intro-card')
+let colorCard = document.getElementById('color-card')
+
+
+function generate(){
+
+    introCard.style.display = 'none'
+    colorCard.style.display = 'block'
+
+}
+
+function generateColors(){
+    
+}
