d3a0ea88bd5ac93405952478a0303af1e9fe6554
m4shaikh <moin.shaikh.3656@gmail.com>
2026-05-19 03:24:19 +0530

    fixed some responsive issues

--- React/Projects/creAtIon/src/components/Footer.jsx
+++ React/Projects/creAtIon/src/components/Footer.jsx
@@ -2,8 +2,8 @@ import { AiOutlineInstagram,AiOutlineX,AiOutlineFacebook,AiOutlineLinkedin } fro
 
 const Footer = () => {
     return (
-        <div className="mt-24 bg-elevated dark:bg-elevated-dark/20 py-4 px-16">
-            <div className="flex place-content-between pb-4 gap-16 border-b-2">
+        <div className="mt-24 bg-elevated dark:bg-elevated-dark/20 py-4 px-16 max-md:px-4">
+            <div className="flex place-content-between pb-4 gap-16 border-b-2 max-md:flex-col">
                 <div className="">
                     <h2 className="mb-2 flex self-center text-2xl text-text-primary dark:text-text-primary-dark font-bold cursor-pointer pb-1">creAtIon</h2>
                     <p className="text-text-secondary dark:text-text-secondary-dark">From strategy to execution, we craft digital solutions that move your business forward.</p>
@@ -17,7 +17,7 @@ const Footer = () => {
                 <div className=" ">
                     <p className="mb-2 text-text-primary dark:text-text-primary-dark text-lg">Subscribe to our newsettler</p>
                     <p className="mb-4 text-text-secondary dark:text-text-secondary-dark">The latest news, articles, and resources, sent to your inbox weekly.</p>
-                    <div className="flex gap-4">
+                    <div className="flex gap-4 max-md:flex-col">
 
                         <input className="dark:bg-elevated-dark w-full focus:outline-none py-2 px-4 border-[1px] rounded-lg " type="email" name="email" id="email" placeholder="Enter your email" />
                         <button className="bg-surface text-text-primary dark:text-text-primary-dark dark:bg-canvas-dark py-2 px-6 rounded-lg border-2 shadow-md ">Submit</button>
@@ -26,7 +26,7 @@ const Footer = () => {
                 </div>
             </div>
 
-            <div className="flex place-content-between items-center pt-2" >
+            <div className="flex max-md:flex-col place-content-between items-center pt-2" >
                 <p className="text-text-secondary dark:text-text-secondary-dark">Copyright 2025 © GreatStack - All Right Reserved.</p>
                 <div className="flex text-3xl gap-3 text-text-primary/60 dark:text-text-primary-dark/60 text-xl">
                     <AiOutlineInstagram/>
